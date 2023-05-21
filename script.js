9e6a12b8fc7954d085ab961cb331cb2bc5434315
Tushar verma <105276375+tusharverma200@users.noreply.github.com>
2023-05-21 02:37:50 -0700

    Add files via upload
    This is my first chrome extension.

---
+++ script.js
@@ -0,0 +1,24 @@
+async function getMatchData(){
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=e72c89af-3b53-46fe-9dfc-722983f7d5fd&offset=0")
+    .then(data => data.json())
+    .then(data => {
+        if (data.status != "success")return;
+
+        const matchesList = data.data;
+
+        if(!matchesList)return [];
+        
+        //add your api key from cricketdata.org
+        const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+        console.log({relevantData});
+
+        document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+        return relevantData;
+
+    })
+    .catch(e => console.log(e));
+}
+
+getMatchData();
